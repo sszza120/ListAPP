523156e229c9f37800c6ca004eb6f7807b7d017a
sszza120 <z875ab@gmail.com>
2020-05-11 12:28:17 +0800

    完成專案

--- app/src/main/java/com/sszza120/listapp/MainActivity.java
+++ app/src/main/java/com/sszza120/listapp/MainActivity.java
@@ -2,12 +2,41 @@ package com.sszza120.listapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView ls;
+    private String[] favorite;
+    private TextView txvshow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ls = findViewById(R.id.ls);
+        txvshow = findViewById(R.id.txvshow);
+
+        favorite = getResources().getStringArray(R.array.favorite);
+        ArrayAdapter<String> ad = new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice, favorite);
+        ls.setAdapter(ad);
+        ls.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+
+    }
+
+    public void btnOK(View view) {
+
+        String str = "";
+        for(int i = 0; i<favorite.length; i++){
+            if (ls.isItemChecked(i))
+                str += favorite[i] + "\n";
+        }
+
+        txvshow.setText(str);
     }
 }
